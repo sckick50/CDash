479b8befa84dee63ca60758898f94ce3b3d827db
Bryon Bean <bryon.bean@kitware.com>
2018-02-10 13:01:14 -0500

    fix: fixes build not setting project's id on lazy load

--- app/Model/Build.php
+++ app/Model/Build.php
@@ -2731,6 +2731,7 @@ class Build
     {
         if (!$this->Project) {
             $this->Project = new Project();
+            $this->Project->Id = $this->ProjectId;
         }
         return $this->Project;
     }
