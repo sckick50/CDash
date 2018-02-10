a0d98b15c8ce3491ba2c948e2b1ca416411da7c3
Bryon Bean <bryon.bean@kitware.com>
2018-02-10 12:52:19 -0500

    refactor: adds Project property and lazy-load accessor/mutator

--- app/Model/Build.php
+++ app/Model/Build.php
@@ -83,6 +83,7 @@ class Build
     private $Failures;
     private $PDO;
     private $Site;
+    private $Project;
     private $BuildConfigure;
 
     public function __construct()
@@ -2721,5 +2722,24 @@ class Build
     public function SetBuildConfigure(BuildConfigure $buildConfigure)
     {
         $this->BuildConfigure = $buildConfigure;
+    }
+
+    /**
+     * @return Project
+     */
+    public function GetProject()
+    {
+        if (!$this->Project) {
+            $this->Project = new Project();
+        }
+        return $this->Project;
+    }
+
+    /**
+     * @param Project $project
+     */
+    public function SetProject(Project $project)
+    {
+        $this->Project = $project;
     }
 }
