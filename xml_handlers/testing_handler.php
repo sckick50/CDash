24cda34c5eeb4a7f8940ddfaca65cc3c395f5875
Bryon Bean <bryon.bean@kitware.com>
2018-02-20 15:17:22 -0500

    refactor: refactor of email body decoration

--- xml_handlers/testing_handler.php
+++ xml_handlers/testing_handler.php
@@ -427,4 +427,9 @@ class TestingHandler extends AbstractHandler implements ActionableBuildInterface
     {
         // TODO: remove
     }
+
+    public function GetSite()
+    {
+        return $this->Site;
+    }
 }
