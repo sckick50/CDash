212424c9b689cfb8be1c6c894dfde98f02f2cb94
Bryon Bean <bryon.bean@kitware.com>
2018-05-23 12:49:44 -0400

    feat: save notifications and check topic for sent

--- app/Model/Build.php
+++ app/Model/Build.php
@@ -19,12 +19,15 @@ include_once 'include/common.php';
 include_once 'include/ctestparserutils.php';
 include_once 'include/repository.php';
 
+use CDash\Collection\BuildEmailCollection;
 use CDash\Collection\DynamicAnalysisCollection;
 use CDash\Collection\LabelCollection;
 use CDash\Config;
 use CDash\Log;
 use CDash\Collection\TestCollection;
 use CDash\Database;
+use CDash\Messaging\Topic\Topic;
+use CDash\Model\ActionableTypes;
 use PDO;
 
 class Build
@@ -94,6 +97,7 @@ class Build
     private $BuildConfigure;
     private $LabelCollection;
     private $DynamicAnalysisCollection;
+    private $BuildEmailCollection;
 
     public function __construct()
     {
@@ -2905,5 +2909,18 @@ class Build
             $this->DynamicAnalysisCollection = new DynamicAnalysisCollection();
         }
         return $this->DynamicAnalysisCollection;
+    }
+
+    public function GetBuildEmailCollection($category)
+    {
+        if (!$this->BuildEmailCollection) {
+            $this->BuildEmailCollection = BuildEmail::GetBuildEmailSent($this->Id, $category);
+        }
+        return $this->BuildEmailCollection;
+    }
+
+    public function SetBuildEmailCollection(BuildEmailCollection $collection)
+    {
+        $this->BuildEmailCollection = $collection;
     }
 }
